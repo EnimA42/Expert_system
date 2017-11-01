bb41314074cf38e8c79f90c17547d58357e1ec8e
Amine DERRAGUI <aderragu@e2r4p21.42.fr>
2017-11-01 18:03:45 +0100

    [+] main filewith first definition of the node class

---
+++ main.py
@@ -0,0 +1,14 @@
+class Node:
+    """Classe that defines a node. Could be a statment or a operation"""
+
+    
+    def __init__(self, symbol, isOps):
+        """
+        """
+        self.symbol = symbol
+        self.isOps = isOps
+        self.
+    def add_link(link):
+        self.links.append(link)
+
+
